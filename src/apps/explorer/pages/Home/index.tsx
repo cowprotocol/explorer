98ce833f4e64b646bce03a443bc9146eba64db0e
Anxo Rodriguez <anxolin@gmail.com>
2021-02-12 09:46:52 +0100

    Cleanup home page (#135)

--- src/apps/explorer/pages/Home/index.tsx
+++ src/apps/explorer/pages/Home/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
+import homeTeaser from './home-teaser.png'
 
 const Wrapper = styled.div`
   overflow: hidden;
@@ -8,13 +9,23 @@ const Wrapper = styled.div`
   height: calc(100vh - var(--height-bar-default));
   position: fixed;
   top: var(--height-bar-default);
+
+  text-align: center;
+  img {
+    max-width: 100%;
+    width: 1200px;
+  }
 `
 
 export const Home: React.FC = () => {
   return (
     <Wrapper>
-      <h1>Gnosis Protocol Explorer</h1>
-      <p>Welcome :)</p>
+      <a
+        href="https://docs.google.com/presentation/d/1a6H06zN4m6eMFpMR4eV2gAtJxW8LB0bb/edit#slide=id.p1"
+        target="blank"
+      >
+        <img src={homeTeaser} />
+      </a>
     </Wrapper>
   )
 }
