803618939b88f41383b52f7533c3871022011a8d
Anxo Rodriguez <anxolin@gmail.com>
2021-03-02 19:11:50 +0100

    Add  init analytics

---
+++ src/api/analytics/index.tsx
@@ -0,0 +1,28 @@
+import ReactGA, { InitializeOptions } from 'react-ga'
+import { isMobile } from 'react-device-detect'
+
+function getCustomBrowserType(): string {
+  if (isMobile) {
+    return 'ethereum' in window ? 'mobileWeb3' : 'mobileRegular'
+  } else {
+    return 'web3' in window ? 'web3' : 'desktop'
+  }
+}
+
+interface InitAnalyticsParams {
+  trackingCode: string
+  options?: InitializeOptions
+}
+
+export function initAnalytics(params: InitAnalyticsParams): void {
+  const { trackingCode, options } = params
+  if (typeof trackingCode === 'string') {
+    ReactGA.initialize(trackingCode, options)
+
+    ReactGA.set({
+      customBrowserType: getCustomBrowserType(),
+    })
+  } else {
+    ReactGA.initialize('test', { testMode: true, debug: true })
+  }
+}
