70fad081b928e0a32f738d5f785d46f7004e6668
Anxo Rodriguez <anxolin@gmail.com>
2021-03-05 14:25:30 +0100

    Add Analytics support (#237)
    * Add Google Analytics
    
    * Add  init analytics
    
    * Init analytics
    
    * Track errors
    
    * Add route analytics
    
    * Rename page view method
    
    * Change env name
    
    * Remove analytics code
    
    * Add env in build stage
    
    * Add enum for analytics dimensions
    
    * Add dimension names per app
    
    * Init dimension always

--- src/apps/explorer/ExplorerApp.tsx
+++ src/apps/explorer/ExplorerApp.tsx
@@ -11,6 +11,16 @@ import { GenericLayout } from 'components/layout'
 import { Header } from './layout/Header'
 
 import { NetworkUpdater } from 'state/network'
+import { initAnalytics } from 'api/analytics'
+import RouteAnalytics from 'components/common/RouteAnalytics'
+import { DIMENSION_NAMES } from './const'
+
+// Init analytics
+const GOOGLE_ANALYTICS_ID: string | undefined = process.env.GOOGLE_ANALYTICS_ID
+initAnalytics({
+  trackingCode: GOOGLE_ANALYTICS_ID,
+  dimensionNames: DIMENSION_NAMES,
+})
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const Router: typeof BrowserRouter & typeof HashRouter = (window as any).IS_IPFS ? HashRouter : BrowserRouter
@@ -55,6 +65,7 @@ const AppContent = (): JSX.Element => {
   return (
     <GenericLayout header={<Header />}>
       <React.Suspense fallback={null}>
+        <Route component={RouteAnalytics} />
         <Switch>
           <Route path={pathPrefix + '/'} exact component={Home} />
           <Route path={pathPrefix + '/orders/:orderId'} exact component={Order} />
