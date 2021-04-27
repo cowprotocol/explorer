0d0b7fdd86d43c5d42fc63c2f64522a30960ce01
Anxo Rodriguez <anxolin@gmail.com>
2021-04-27 13:21:00 +0200

    Uncomment

--- .github/scripts/prepare_production_deployment.sh
+++ .github/scripts/prepare_production_deployment.sh
@@ -8,12 +8,11 @@ echo "🚀 Preparing deploy for version $VERSION_TAG. Calling hook Url: $PROD_DE
 
 if [ -n "$VERSION_TAG" ] && [ -n "$PROD_DEPLOYMENT_HOOK_TOKEN" ] && [ -n "$PROD_DEPLOYMENT_HOOK_URL" ]
 then
-  echo "Calling hook"
-  # curl --silent --output /dev/null --write-out "%{http_code}" -X POST \
-  #    -F token="$PROD_DEPLOYMENT_HOOK_TOKEN" \
-  #    -F ref=master \
-  #    -F "variables[TRIGGER_RELEASE_COMMIT_TAG]=$VERSION_TAG" \
-  #     $PROD_DEPLOYMENT_HOOK_URL
+  curl --silent --output /dev/null --write-out "%{http_code}" -X POST \
+     -F token="$PROD_DEPLOYMENT_HOOK_TOKEN" \
+     -F ref=master \
+     -F "variables[TRIGGER_RELEASE_COMMIT_TAG]=$VERSION_TAG" \
+      $PROD_DEPLOYMENT_HOOK_URL
 else
   echo  >&2 "[ERROR] Deployment to production could not be prepared. Some Environment variables are missing"
   exit 100
