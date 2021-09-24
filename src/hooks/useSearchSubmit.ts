4122e28547d04af448d1d77e95468f656a300f44
stackseeder <79759622+stackseeder@users.noreply.github.com>
2021-09-24 23:48:25 +0200

    Order not found UI (#658)
    # Summary
    Closes #641 
    Create a Order not found page result.
    Design:
    https://www.figma.com/file/m7obOydxAhGNmlFYvxTk3Z/GP-Explorer?node-id=1144%3A1416
    
    # To Test
    1. Search for example: "0xb6BAd41ae76A11D10f7b0E664C5007b908bC77C9" in the [order box search](https://pr658--gpui.review.gnosisdev.com/) (home).
    2. It should display the ["Order not found" page](https://pr658--gpui.review.gnosisdev.com/orders/0xb6BAd41ae76A11D10f7b0E664C5007b908bC77C9). 
    
    ![image](https://user-images.githubusercontent.com/622217/134690973-3c038fd9-034d-4223-8196-3868e5685e95.png)
    
    ![image](https://user-images.githubusercontent.com/622217/134691021-dc97f2a6-4031-4e2a-8c17-0c0310895f9d.png)

--- src/hooks/useSearchSubmit.ts
+++ src/hooks/useSearchSubmit.ts
@@ -1,18 +1,15 @@
 import { useCallback } from 'react'
-import { useHistory, useRouteMatch } from 'react-router-dom'
+import { useHistory } from 'react-router-dom'
 
 export function useSearchSubmit(): (query: string) => void {
   const history = useHistory()
-  const { path } = useRouteMatch()
 
   return useCallback(
     (query: string) => {
-      const pathPrefix = path.endsWith('/') ? path : path + '/'
-
       // For now assumes /orders/ path. Needs logic to try all types for a valid response:
       // Orders, transactions, tokens, batches
-      query && query.length > 0 && history.push(`${pathPrefix}orders/${query}`)
+      query && query.length > 0 && history.push(`/orders/${query}`)
     },
-    [history, path],
+    [history],
   )
 }
