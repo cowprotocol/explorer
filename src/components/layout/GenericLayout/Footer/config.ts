a83598334bc51de492fd05def5d7b31f06cb7d0f
Henry Palacios <henrydpalacios@gmail.com>
2022-04-05 14:24:49 -0300

    Update URLs to new repositories

--- src/components/layout/GenericLayout/Footer/config.ts
+++ src/components/layout/GenericLayout/Footer/config.ts
@@ -1,13 +1,13 @@
 export const footerConfig = {
   isBeta: false,
   url: {
-    web: 'https://github.com/gnosis/gp-ui/tree/v',
+    web: 'https://github.com/cowprotocol/explorer/tree/v',
     // TODO: Pending to move and adapt the wiki
     appId: null,
     // appId: 'https://github.com/gnosis/gp-v1-ui/wiki/App-Ids-for-Forks',
     contracts: {
-      settlement: `https://github.com/gnosis/gp-v2-contracts/blob/v${CONTRACT_VERSION}/src/contracts/GPv2Settlement.sol`,
-      vaultRelayer: `https://github.com/gnosis/gp-v2-contracts/blob/v${CONTRACT_VERSION}/src/contracts/GPv2VaultRelayer.sol`,
+      settlement: `https://github.com/cowprotocol/contracts/blob/v${CONTRACT_VERSION}/src/contracts/GPv2Settlement.sol`,
+      vaultRelayer: `https://github.com/cowprotocl/contracts/blob/v${CONTRACT_VERSION}/src/contracts/GPv2VaultRelayer.sol`,
     },
   },
 }
