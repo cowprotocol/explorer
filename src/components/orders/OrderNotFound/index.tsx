f1d3935729491d0ad8a76ad7f7d36fc211719623
Henry Palacios <henrydpalacios@gmail.com>
2022-04-05 14:34:12 -0300

    Update chat support

--- src/components/orders/OrderNotFound/index.tsx
+++ src/components/orders/OrderNotFound/index.tsx
@@ -120,7 +120,7 @@ export const OrderAddressNotFound: React.FC = (): JSX.Element => {
           <SearchContent>
             <Search searchString={wasRedirected ? '' : searchString} submitSearchImmediatly={!wasRedirected} />
             <span>or</span>
-            <Support href="https://chat.cowswap.exchange/" target="_blank" rel="noopener noreferrer">
+            <Support href="https://discord.gg/cowprotocol/" target="_blank" rel="noopener noreferrer">
               Get Support
               <img src={SupportIcon} />
             </Support>
