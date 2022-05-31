9c60af0fa333be8e20af6893b2a6517c216be4c9
Henry Palacios <henrydpalacios@gmail.com>
2022-05-30 21:58:19 -0300

    Adding surplus column on tx table (#99)
    * Adding surplus column on tx table
    
    * Use popper tooltip with click instead hover
    
    * Fixing onClick on tooltip
    
    * Creating BaseIconTooltip
    
    * Avoid use tooltip on mobile view
    
    * Fix mobile view wrap surplus
    
    * Use OrderSurplusTooltipDisplay on OrderUserTable
    
    * Hover instead Click, no cast return, amount of smart formatting
    
    * Fix row spacing, improve type naming

--- src/components/orders/OrderSurplusDisplay/index.tsx
+++ src/components/orders/OrderSurplusDisplay/index.tsx
@@ -1,13 +1,23 @@
 import React from 'react'
-import styled from 'styled-components'
+import styled, { useTheme } from 'styled-components'
 
 import { Order } from 'api/operator'
 
-import { formatSmart, formatSmartMaxPrecision, safeTokenName } from 'utils'
+import {
+  formatSmart,
+  formatSmartMaxPrecision,
+  safeTokenName,
+  formattingAmountPrecision,
+  FormatAmountPrecision,
+} from 'utils'
 
 import { LOW_PRECISION_DECIMALS, PERCENTAGE_PRECISION } from 'apps/explorer/const'
+import { BaseIconTooltipOnHover } from 'components/Tooltip'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowAltCircleUp as faIcon } from '@fortawesome/free-regular-svg-icons'
 
 const Wrapper = styled.div`
+  display: flex;
   & > * {
     margin-right: 0.25rem;
   }
@@ -26,12 +36,11 @@ const Surplus = styled.span`
 //   opacity: 0.5;
 // `
 
-export type Props = { order: Order }
+export type Props = { order: Order; amountSmartFormatting?: boolean } & React.HTMLAttributes<HTMLDivElement>
+type SurplusText = { amount: string; percentage: string; formattedSmartAmount: string }
 
-export function OrderSurplusDisplay(props: Props): JSX.Element | null {
-  const {
-    order: { kind, buyToken, sellToken, surplusAmount, surplusPercentage },
-  } = props
+function useGetSurplus(order: Order): SurplusText | null {
+  const { kind, buyToken, sellToken, surplusAmount, surplusPercentage } = order
 
   const surplusToken = kind === 'buy' ? sellToken : buyToken
 
@@ -47,7 +56,13 @@ export function OrderSurplusDisplay(props: Props): JSX.Element | null {
     precision: PERCENTAGE_PRECISION,
     decimals: LOW_PRECISION_DECIMALS,
   })
-  const formattedSurplusAmount = formatSmartMaxPrecision(surplusAmount, surplusToken)
+  const formattedSurplusAmountMaxPrecision = formatSmartMaxPrecision(surplusAmount, surplusToken)
+  const formattedSurplusAmount = formattingAmountPrecision(
+    surplusAmount,
+    surplusToken,
+    FormatAmountPrecision.highPrecision,
+  )
+
   const tokenSymbol = safeTokenName(surplusToken)
   // const formattedUsdAmount = formatSmart({
   //   amount: usdAmount,
@@ -55,13 +70,52 @@ export function OrderSurplusDisplay(props: Props): JSX.Element | null {
   //   decimals: LOW_PRECISION_DECIMALS,
   // })
 
+  return {
+    amount: `${formattedSurplusAmountMaxPrecision} ${tokenSymbol}`,
+    formattedSmartAmount: `${formattedSurplusAmount} ${tokenSymbol}`,
+    percentage: `+${formattedSurplusPercentage}%`,
+  }
+}
+
+export function OrderSurplusDisplay(props: Props): JSX.Element | null {
+  const surplus = useGetSurplus(props.order)
+
+  if (!surplus) return null
+
   return (
-    <Wrapper>
-      <Surplus>+{formattedSurplusPercentage}%</Surplus>
-      <span>
-        {formattedSurplusAmount} {tokenSymbol}
-      </span>
+    <Wrapper className={props.className}>
+      <Surplus>{surplus.percentage}</Surplus>
+      <span>{props.amountSmartFormatting ? surplus.formattedSmartAmount : surplus.amount}</span>
       {/* <UsdAmount>(~${formattedUsdAmount})</UsdAmount> */}
     </Wrapper>
+  )
+}
+
+const IconWrapper = styled(FontAwesomeIcon)`
+  padding: 0.6rem;
+  margin: -0.6rem 0 -0.6rem -0.6rem;
+  box-sizing: content-box;
+
+  :hover {
+    cursor: pointer;
+  }
+`
+
+export function OrderSurplusTooltipDisplay({ order }: Props): JSX.Element | null {
+  const surplus = useGetSurplus(order)
+  const theme = useTheme()
+
+  if (!surplus) return null
+
+  return (
+    <BaseIconTooltipOnHover
+      tooltip={surplus.amount}
+      targetContent={
+        <span>
+          <IconWrapper icon={faIcon} color={theme.green} />
+          <Surplus>{surplus.percentage}</Surplus>
+        </span>
+      }
+    />
   )
 }
