3bb2446a8dd253f2cf75a4c671322847168a9d18
Agustín Longoni <agustin.longoni@altoros.com>
2022-06-03 18:22:20 -0300

    [Batch Viewer] Add a Reset button + autoscale graph area (#100)
    * add basic reset button
    
    * add styles to resert button
    
    * styles on mobile
    
    * Use just one useEffect and fit grpah after height resize
    
    * add z-index to reset button
    
    * Avoiding memory leaks unlistening events
    
    * Removing all listeners when unmounted
    
    * Moving declarations to types/cytoscape.d.ts
    
    Co-authored-by: Henry Palacios <henrydpalacios@gmail.com>

---
+++ src/types/cytoscape.d.ts
@@ -0,0 +1,9 @@
+/* eslint-disable-next-line @typescript-eslint/no-unused-vars */
+import { Core } from 'types/cytoscape'
+
+// Type declaration to module augmentation
+declare module 'cytoscape' {
+  interface Core {
+    removeAllListeners: () => void
+  }
+}
