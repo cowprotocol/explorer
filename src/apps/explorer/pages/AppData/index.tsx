63a0a62ced0ace9fc3e42ab2e781151449d32d90
Agustín Longoni <agustin.longoni@altoros.com>
2022-08-04 13:54:02 -0300

    rm duplicate icon

--- src/apps/explorer/pages/AppData/index.tsx
+++ src/apps/explorer/pages/AppData/index.tsx
@@ -1,6 +1,6 @@
 import React, { useCallback, useState, useEffect } from 'react'
 import { useHistory } from 'react-router-dom'
-import { faCode, faListUl, faCode } from '@fortawesome/free-solid-svg-icons'
+import { faCode, faListUl } from '@fortawesome/free-solid-svg-icons'
 import { useQuery } from 'hooks/useQuery'
 import FormPage from './FormPage'
 import DecodePage from './DecodePage'
