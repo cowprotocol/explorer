65c3483175842401a8f5cd11ce6a75b2f8a76b7a
Agustín Longoni <agustin.longoni@altoros.com>
2022-08-18 16:44:48 -0300

    add menu content

--- src/components/common/MenuDropdown/MenuDropdown.stories.tsx
+++ src/components/common/MenuDropdown/MenuDropdown.stories.tsx
@@ -20,18 +20,40 @@ const itemContent = {
       sectionTitle: 'OVERVIEW',
       links: [
         {
-          title: 'Overview',
+          title: 'CoW Protocol',
           url: 'https://cow.fi',
           /* icon?: string, // If icon uses a regular <img /> tag */
           /*  iconSVG?: string // If icon is a <SVG> inline component */
         },
         {
-          title: 'Overview2',
-          url: 'https://cow.fi',
+          title: 'Documentation',
+          url: 'https://docs.cow.fi',
         },
         {
-          title: 'Overview3',
-          url: 'https://cow.fi',
+          title: 'Analytics',
+          url: 'https://dune.xyz/gnosis.protocol/Gnosis-Protocol-V2',
+        },
+      ],
+    },
+    {
+      sectionTitle: 'COMMUNITY',
+      links: [
+        {
+          title: 'Discord',
+          url: 'https://discord.gg/cowprotocol',
+          /* icon?: string, // If icon uses a regular <img /> tag */
+          /*  iconSVG?: string // If icon is a <SVG> inline component */
+        },
+      ],
+    },
+    {
+      sectionTitle: 'OTHER',
+      links: [
+        {
+          title: 'App Data',
+          url: '#',
+          /* icon?: string, // If icon uses a regular <img /> tag */
+          /*  iconSVG?: string // If icon is a <SVG> inline component */
         },
       ],
     },
