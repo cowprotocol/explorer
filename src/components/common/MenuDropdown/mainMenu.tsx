7e5d7f6977543d4726116f8a2a767af650d05946
Mati Dastugue <mdastugu@amazon.com>
2022-09-02 15:22:07 -0300

    Convert file to .tsx

--- src/components/common/MenuDropdown/mainMenu.tsx
+++ src/components/common/MenuDropdown/mainMenu.tsx
@@ -1,7 +1,8 @@
-import { DOCS_LINK, DISCORD_LINK, PROTOCOL_LINK, DUNE_DASHBOARD_LINK, Routes } from 'apps/explorer/const'
+import React from 'react'
 
-// import IMAGE_DISCORD from 'assets/img/discord.svg'
-// import LogoWrapper, { LOGO_MAP } from 'components/common/LogoWrapper'
+import { DOCS_LINK, DISCORD_LINK, PROTOCOL_LINK, DUNE_DASHBOARD_LINK, Routes } from 'apps/explorer/const'
+import IMAGE_DISCORD from 'assets/img/discord.svg'
+import LogoWrapper, { LOGO_MAP } from 'components/common/LogoWrapper'
 
 export const menuContent = [
   {
@@ -37,8 +38,8 @@ export const menuContent = [
           {
             title: 'Discord',
             url: DISCORD_LINK,
-            // icon: <LogoWrapper className="github-logo" src={LOGO_MAP.github} title="Open it on Github" />, // If icon uses a regular <img /> tag */
-            // iconSVG: IMAGE_DISCORD, // If icon is a <SVG> inline component
+            icon: <LogoWrapper className="github-logo" src={LOGO_MAP.github} title="Open it on Github" />, // If icon uses a regular <img /> tag */
+            iconSVG: IMAGE_DISCORD, // If icon is a <SVG> inline component
           },
         ],
       },
