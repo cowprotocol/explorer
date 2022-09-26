7ab5f4e7ef673adcd59bd1d86676e656e14e4673
Agustín Longoni <agustin.longoni@altoros.com>
2022-09-26 15:49:16 -0300

    update cowLoading component

--- src/components/common/CowLoading.tsx
+++ src/components/common/CowLoading.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import styled, { keyframes } from 'styled-components'
 import SVG from 'react-inlinesvg'
-import CowLoadingSVG from 'assets/img/cowLoading.svg'
+import CowLoadingSVG from 'assets/img/CoW-loading.svg'
 
 export const WrapperCenter = styled.div`
   display: flex;
@@ -10,45 +10,21 @@ export const WrapperCenter = styled.div`
   height: 100%;
 `
 
-const CowBounce = keyframes`
+const CowAnimation = keyframes`
   0%,
   100% {
     transform: scale(0.95) translateX(1px);
+    opacity: 0.4;    
   }
   50% {
     transform: scale(1);
-  }
-`
-
-const EyesOpacity = keyframes`
-  from {
-    opacity: 1;
-  }
-  30% {
-    opacity: 0.3;
-  }
-  to {
     opacity: 1;
   }
 `
-
 export const StyledCowLoading = styled(SVG)`
   .cowLoading {
-    animation: ${CowBounce} 1.5s infinite ease-in-out;
+    animation: ${CowAnimation} 1.4s infinite ease-in-out;
     animation-delay: -1s;
-  }
-  .cowHead {
-    fill: ${({ theme }): string => theme.white};
-    opacity: 0.4;
-  }
-  .eyesBg {
-    fill: ${({ theme }): string => theme.bg1};
-    opacity: 1;
-  }
-  .eyes {
-    fill: ${({ theme }): string => theme.orange};
-    animation: ${EyesOpacity} 1.5s ease-in-out infinite;
-    filter: blur(1px);
   }
 `
 
