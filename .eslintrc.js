d28de325f4e34545b0c3970119ccb325e6db0744
Alfetopito <alfetopito@users.noreply.github.com>
2022-12-30 09:00:06 +0000

    Re-formatted eslintrc.js file

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,52 +1,70 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
   // Specifies the ESLint parser
-  extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:@typescript-eslint/recommended', 'prettier/@typescript-eslint', 'plugin:prettier/recommended', 'plugin:storybook/recommended'],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:storybook/recommended',
+    'plugin:prettier/recommended',
+  ],
   parserOptions: {
     ecmaVersion: 2018,
     // Allows for the parsing of modern ECMAScript features
     sourceType: 'module',
     // Allows for the use of imports
     ecmaFeatures: {
-      jsx: true // Allows for the parsing of JSX
-    }
+      jsx: true, // Allows for the parsing of JSX
+    },
   },
 
   rules: {
     // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
     // e.g. "@typescript-eslint/explicit-function-return-type": "off",
-    '@typescript-eslint/explicit-function-return-type': ['error', {
-      allowTypedFunctionExpressions: true
-    }],
-    'react/prop-types': [1, {
-      skipUndeclared: true
-    }],
+    '@typescript-eslint/explicit-function-return-type': [
+      'error',
+      {
+        allowTypedFunctionExpressions: true,
+      },
+    ],
+    'react/prop-types': [
+      1,
+      {
+        skipUndeclared: true,
+      },
+    ],
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': 'warn',
-    '@typescript-eslint/no-unused-vars': ['error', {
-      ignoreRestSiblings: true
-    }]
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        ignoreRestSiblings: true,
+      },
+    ],
   },
-  overrides: [{
-    files: ['**/*.js'],
-    rules: {
-      '@typescript-eslint/explicit-function-return-type': 0,
-      '@typescript-eslint/no-var-requires': 0
-    }
-  }],
+  overrides: [
+    {
+      files: ['**/*.js'],
+      rules: {
+        '@typescript-eslint/explicit-function-return-type': 0,
+        '@typescript-eslint/no-var-requires': 0,
+      },
+    },
+  ],
   plugins: ['react-hooks', '@typescript-eslint'],
   settings: {
     react: {
-      version: 'detect' // Tells eslint-plugin-react to automatically detect the version of React to use
-    }
+      version: 'detect', // Tells eslint-plugin-react to automatically detect the version of React to use
+    },
   },
 
   globals: {
-    VERSION: true
+    VERSION: true,
   },
   env: {
     browser: true,
     node: true,
-    jest: true
-  }
-};
+    jest: true,
+  },
+}
