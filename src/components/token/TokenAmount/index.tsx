c38e42734639a69192863a44be191be40880b7f3
Alexandr Kazachenko <shoom3301@gmail.com>
2023-04-06 17:42:50 +0600

    Fix lint errors

--- src/components/token/TokenAmount/index.tsx
+++ src/components/token/TokenAmount/index.tsx
@@ -3,11 +3,21 @@ import BigNumber from 'bignumber.js'
 import { TokenErc20 } from '@gnosis.pm/dex-js'
 import { FormatAmountPrecision, formatSmartMaxPrecision, formattingAmountPrecision } from 'utils'
 
-// TODO: unify with TokenAmount in @cowprotocol/cowswap
-export function TokenAmount({ amount, token, symbol }: { amount: BigNumber, symbol?: string, token?: TokenErc20 | null }) {
-    const fullAmount = formatSmartMaxPrecision(amount, token || null)
-    const displayedAmount = formattingAmountPrecision(amount, token || null, FormatAmountPrecision.highPrecision)
-    const displayedSymbol = symbol || token?.symbol
+interface Props {
+  amount: BigNumber
+  symbol?: string
+  token?: TokenErc20 | null
+}
 
-    return <span title={fullAmount + ' ' + displayedSymbol}>{displayedAmount} {displayedSymbol}</span>
+// TODO: unify with TokenAmount in @cowprotocol/cowswap
+export const TokenAmount: React.FC<Props> = ({ amount, token, symbol }: Props) => {
+  const fullAmount = formatSmartMaxPrecision(amount, token || null)
+  const displayedAmount = formattingAmountPrecision(amount, token || null, FormatAmountPrecision.highPrecision)
+  const displayedSymbol = symbol || token?.symbol
+
+  return (
+    <span title={fullAmount + ' ' + displayedSymbol}>
+      {displayedAmount} {displayedSymbol}
+    </span>
+  )
 }
