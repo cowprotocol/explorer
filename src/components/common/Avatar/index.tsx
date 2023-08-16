5d6362a91089df1ea68b84256314f636b0869580
Vinicius Gamaliel <vicebas@gmail.com>
2023-08-16 16:47:05 -0300

    avatar boring colors

--- src/components/common/Avatar/index.tsx
+++ src/components/common/Avatar/index.tsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Avatar } from '@material-ui/core'
 
 type BoringAvatarProps = {
   alt?: string
 }
-const BoringAvatar: React.FC<BoringAvatarProps> = ({ alt }) => (
-  <Avatar alt={alt} src={`https://source.boringavatars.com/pixel/120/${alt}`} />
-)
-
+const BoringAvatar: React.FC<BoringAvatarProps> = ({ alt }) => {
+  const [colors] = useState<string[]>(
+    Array.from({ length: 6 }, () => Math.floor(Math.random() * 16777215).toString(16)),
+  )
+  return <Avatar alt={alt} src={`https://source.boringavatars.com/pixel/120/${alt}?colors=${colors.join(',')}`} />
+}
 export default BoringAvatar
